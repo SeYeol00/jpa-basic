d3cc61f2bd52e11b8582b3dc4e9468e8321d7c5f
SeYeol_Park <79959576+SeYeol00@users.noreply.github.com>
2022-12-19 21:46:06 +0900

    Update JpaMain.java

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -25,11 +26,32 @@ public class JpaMain {
 //            member.setName("HelloA");
 //            em.persist(member);
 
-            // 조회
-            Member member = em.find(Member.class, 1L);
+//            // 조회
+//            Member member = em.find(Member.class, 1L);
+//
+//            // 수정 , 자바 컬렉션처럼 쓰는 거라 persist()를 안 해도 된다.
+//            member.setName("HelloJPA");
+//
+//            // JPQL 사용
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(5) // limit
+//                    .setMaxResults(8) // offset JPQL은 페이징이 이렇게 쉽다.
+//                    .getResultList();
+//
+//            for (Member member1 : result) {
+//                System.out.println("member1 = " + member1.getName());
+//            }
 
-            // 수정 , 자바 컬렉션처럼 쓰는 거라 persist()를 안 해도 된다.
+            // 비영속
+            Member member = new Member();
+            member.setId(100L);
             member.setName("HelloJPA");
+
+            // 영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
 
 
             // 트랜잭션 끝, 정상이면 커밋
@@ -39,6 +61,8 @@ public class JpaMain {
             tx.rollback();
             throw e;
         }finally {
+            // 엔티티 매니저는 쓰레드 간에 공유를 하면 안 된다.
+            // 쓰레드가 끝날 떄 close()를 해주어야한다.
             em.close();
         }
         emf.close();
